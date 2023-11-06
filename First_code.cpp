a4b87faca55f9ea0692361635dac0ee7350fa64b
mohummednouman <mohummednouman@icloud.com>
2023-11-06 19:58:46 +0530

    Projects

---
+++ First_code.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main  () {
+    cout<<67<<'\n'<<"57.56"<<'\n'<<'M'<<'\n'<<"Hello";
+    return 0;
+}
---
+++ Simple_interest_CAL.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float P,R,T;
+    cin>>P>>R>>T;
+    float SI=P*R*T/100;
+    cout<<"Simple Interest = "<<SI;
+    return 0;
+
+}
