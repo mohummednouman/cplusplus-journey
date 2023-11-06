b00ea174f9970eeffb67013fbac21af5db4c92e5
mohummednouman <mohummednouman@icloud.com>
2023-11-06 20:12:27 +0530

    project

---
+++ Arthematic_OP.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main () {
+//Addition
+    float A,B;
+    cin>>A>>B;
+    float sum = A+B;
+    cout<<sum;
+//Subtraction
+    float C,D;
+    cin>>C>>D;
+    float diff= C-D;
+    cout<<diff;
+//Multipilication
+    float E,F;
+    cin>>E>>F;
+    float mul=E*F;
+    cout<<mul;
+//Division
+    float G,H;
+    cin>>G>>H;
+    float div=G/H;
+    cout<<div;
+return 0;
+}
